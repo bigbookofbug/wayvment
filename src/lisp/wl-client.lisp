e137f5c50517b3844baa7fd91cf630acb5742927
emma thompson <bigbookofbug@proton.me>
2024-10-22 18:01:39 -0500

    client and server lisp files

---
+++ src/lisp/wl-client.lisp
@@ -0,0 +1,36 @@
+(in-package #:wayvment-client)
+
+(defvar +envar-wl-display-xdg+ (uiop:strcat
+								 (uiop:getenv "XDG_RUNTIME_DIR")
+								 "/"
+								 (uiop:getenv "WAYLAND_DISPLAY")))
+
+(defvar +envar-wl-display-xdg-wl-0+
+  (uiop:strcat (uiop:getenv "XDG_RUNTIME_DIR") "/wayland-0"))
+
+(defun display-connect (&optional name)
+  "Where NAME is the name of the socket connecting to. If NIL,
+will be set to '$WAYLAND_DISPLAY' or '$XDG_RUNTIME_DIR/wayland-0'
+Returns a pointer to display connected to. It is discouraged to
+use this function directly unless necessary. In most cases, use
+`with-display`."
+  (if (not name)
+	  (wayvment.ffi-client::wl-display-connect (or +envar-wl-display-xdg+
+												   +envar-wl-display-xdg-wl-0+))
+	  (wayvment.ffi-client::wl-display-connect name)))
+
+(defun display-disconnect (display)
+  "Where DISPLAY is a pointer to the display connected to.
+It is discouraged to use this function directly unless necessary.
+In most cases, use `with-display`."
+  (wayvment.ffi-client::wl-display-disconnect display))
+
+(defun display-roundtrip (display)
+  "Where DISPLAY is the display-name connecting to, or 'wayland-0'.
+Blocks client until the server has processed all currently issued
+requests by sending a request to DISPLAY and awaiting a response.
+`wl-display-roundtrip returns -1 on failure, so we set it accordingly."
+  (let ((res (wayvment.ffi-client:wl-display-roundtrip display)))
+	(if (minusp res)
+		nil
+		t)))
---
+++ src/lisp/wl-server.lisp
@@ -0,0 +1,43 @@
+;;;; wl-server.lisp
+(in-package #:wayvment-server)
+
+(defun display-destroy (display)
+  (wayvment.ffi-server:wl-display-destroy display)
+  (setf display nil))
+
+(defun display-create ()
+  "Allocates a wayland display on foreign memory and
+returns a pointer to that memory.
+Generally discouraged to use directly, as it can be unsafe.
+Unless not possible, use `with-display`."
+  (wayvment.ffi-server:wl-display-create))
+
+(defmacro with-display (display &body body)
+  `(let ((,display (wayvment.ffi-server:wl-display-create)))
+	 (if (cffi:null-pointer-p ,display)
+		 (format t "Failed to create display!")
+		 (unwind-protect
+			  (progn
+			  ,@body)
+		   (display-destroy ,display)))))
+
+(defun display-add-socket (display &optional sock)
+  "Where DISPLAY is the pointer returned from `display-create`,
+or assigned to DISPLAY in `with-display`, and SOCK is either a
+string naming the socket to add, an integer naming the FD to get
+the socket from, or to auto-add a socket."
+  (cond ((integerp sock)
+		 (let ((val (wayvment.ffi-server:wl-display-add-socket-fd display sock)))
+		   (if (minusp val)
+			   (format t "Failed to bind to socket!")
+			   val)))
+		((stringp sock)
+		 (let ((val (wayvment.ffi-server:wl-display-add-socket display sock)))
+		   (if (minusp val)
+			   (format t "Failed to create socket!")
+			   val)))
+		(t
+		 (let ((val (wayvment.ffi-server:wl-display-add-socket-auto display)))
+		   (if (not val)
+			   (format t "Failed to create socket!")
+			   val)))))
